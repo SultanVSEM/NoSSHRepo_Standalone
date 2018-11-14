67c8d6ebfb936562e47e133e209dd67b72c4db22
Sultan Sikkandar <ssikkand@ford.com>
2018-11-14 12:37:25 -0500

    Update test.java
    initial commit

---
+++ test.java
@@ -0,0 +1,23 @@
+/**
+* @wi.implements elibrary/EL-115:164 Returns author of this book
+*/
+/* This is a simple Java program. 
+FileName : "HelloWorld.java". */
+class HelloWorld 
+{ 
+	// Your program begins with a call to main(). 
+	// Prints "Hello, World" to the terminal window. 
+	public static void main(String args[]) 
+	{ 
+		System.out.println("Hello, Sultan S"); 
+		
+	} 
+} 
+
+/**
+* {@wi.implements elibrary_alm/EL-122 messages can take up 
+* several lines in the code. Just surround the whole parsing syntax 
+* with {tested}  
+* Anything commented after will not be included.
+*/ 
+      
